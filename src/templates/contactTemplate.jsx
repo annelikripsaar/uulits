30aad09634cd744b15e4352298f5392b635781ae
Anneli Kripsaar <annelikripsaar@macbook-pro.lan>
2020-08-23 21:51:04 +0300

    fix map load

--- src/templates/contactTemplate.jsx
+++ src/templates/contactTemplate.jsx
@@ -3,10 +3,39 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import ContactForm from "../components/Form/Form"
 import styled from "@emotion/styled"
+import { useEffect } from "react"
+
+const Container = styled.div`
+  max-width: 1000px;
+  margin: 64px auto;
+`
 
 const ContactRow = styled.div`
   display: flex;
   align-items: flex-end;
+  margin-bottom: 64px;
+  flex-wrap: wrap;
+
+  div:nth-child(2) {
+    width: 30%;
+  }
+`
+
+const Column = styled.div`
+  margin-left: 40px;
+  line-height: 1.5;
+
+  p:first-of-type {
+    margin-top: 0;
+  }
+
+  h2 {
+    font-size: 32px;
+    line-height: 1;
+    text-transform: uppercase;
+    margin-bottom: 16px;
+    margin-top: 0;
+  }
 `
 
 const TextContainer = styled.div`
@@ -30,22 +59,6 @@ const TextContainer = styled.div`
   line-height: 1.5;
 `
 
-const Column = styled.div`
-  margin-left: 56px;
-  line-height: 1.5;
-
-  p:first-of-type {
-    margin-top: 0;
-  }
-
-  h2 {
-    font-size: 32px;
-    text-transform: uppercase;
-    margin-bottom: 16px;
-    margin-top: 0;
-  }
-`
-
 const Address = styled.span`
   font-weight: 700;
   &::before {
@@ -66,53 +79,110 @@ const kadaka = { lat: 59.41213, lng: 24.668579 }
 const balta = { lat: 59.441073, lng: 24.736337 }
 const tartu = { lat: 58.380947, lng: 26.722218 }
 
+let resolveGoogleMapsSdkPromise
+const googleMapsSdkPromise = new Promise(resolve => {
+  resolveGoogleMapsSdkPromise = resolve
+})
+
 if (typeof window !== "undefined") {
-  window.initMap = function () {
-    const sooMap = new window.google.maps.Map(
-      document.getElementById("sookontakt"),
-      {
-        center: soo,
-        zoom: 15,
-      }
-    )
-    new window.google.maps.Marker({
-      position: soo,
-      map: sooMap,
-    })
-  }
+  window.initMap = resolveGoogleMapsSdkPromise
+}
+
+function initializeMaps() {
+  const sooMap = new window.google.maps.Map(
+    document.getElementById("sookontakt"),
+    {
+      center: soo,
+      zoom: 15,
+    }
+  )
+  new window.google.maps.Marker({
+    position: soo,
+    map: sooMap,
+  })
+
+  const kadakaMap = new window.google.maps.Map(
+    document.getElementById("kadakakontakt"),
+    {
+      center: kadaka,
+      zoom: 15,
+    }
+  )
+  new window.google.maps.Marker({
+    position: kadaka,
+    map: kadakaMap,
+  })
+
+  const baltaMap = new window.google.maps.Map(
+    document.getElementById("baltakontakt"),
+    {
+      center: balta,
+      zoom: 15,
+    }
+  )
+  new window.google.maps.Marker({
+    position: balta,
+    map: baltaMap,
+  })
+
+  const tartuMap = new window.google.maps.Map(
+    document.getElementById("tartukontakt"),
+    {
+      center: tartu,
+      zoom: 15,
+    }
+  )
+  new window.google.maps.Marker({
+    position: tartu,
+    map: tartuMap,
+  })
 }
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { page, sections } = data
+  useEffect(() => {
+    let aborted = false
+
+    googleMapsSdkPromise.then(() => {
+      if (!aborted) {
+        initializeMaps()
+      }
+    })
+    return () => {
+      aborted = true
+    }
+  }, [])
   return (
     <Layout>
-      <TextContainer
-        className="blog-post-content"
-        dangerouslySetInnerHTML={{ __html: page.html }}
-      />
-      {sections.edges.map(section => {
-        return (
-          <ContactRow>
-            <Column>
-              <Map id={section.node.frontmatter.slug}></Map>
-            </Column>
-            <Column>
-              <h2>{section.node.frontmatter.title}</h2>
-              <p>
-                <Address>{section.node.frontmatter.address}</Address>
-                <br />
-                {section.node.frontmatter.address_extra}
-              </p>
-            </Column>
-            <Column
-              key={section.node.id}
-              dangerouslySetInnerHTML={{ __html: section.node.html }}
-            />
-          </ContactRow>
-        )
-      })}
+      <Container>
+        <TextContainer
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: page.html }}
+        />
+        {sections.edges.map(section => {
+          return (
+            <ContactRow key={section.node.id}>
+              <Column>
+                <Map id={section.node.frontmatter.slug}></Map>
+              </Column>
+              <Column>
+                <h2>{section.node.frontmatter.title}</h2>
+                <p>
+                  <Address>{section.node.frontmatter.address}</Address>
+                  <br />
+                  {section.node.frontmatter.address_extra}
+                </p>
+              </Column>
+              <Column
+                key={section.node.id}
+                dangerouslySetInnerHTML={{ __html: section.node.html }}
+              />
+            </ContactRow>
+          )
+        })}
+      </Container>
       <ContactForm />
     </Layout>
   )
