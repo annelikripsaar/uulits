4873f22946314e87fcfe3ffa9258a921dfa62fc4
Anneli Kripsaar <annelikripsaar@MacBook-Pro.tatari.platvorm.ee>
2020-10-27 15:41:16 +0200

    console log

--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -74,6 +74,8 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
 
+  console.log(frontmatter.video)
+
   return (
     <Layout>
       <Columns>
