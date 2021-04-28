6e2f9646c3ac820edc667e4ec0d3787c6d7a8aea
Anneli Kripsaar <annelikripsaar@gmail.com>
2021-04-28 20:20:42 +0300

    Add shop button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,188 +1,219 @@
-import React, { useState } from "react"
-import { useStaticQuery, graphql, Link } from "gatsby"
-import TopBar from "./TopBar"
-import { Social } from "./Social"
-import styled from "@emotion/styled"
-import { screenSize } from "../styles/screenSize"
+import React, { useState } from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import TopBar from './TopBar';
+import { Social } from './Social';
+import styled from '@emotion/styled';
+import { screenSize } from '../styles/screenSize';
 
-const Container = styled.div``
+const Container = styled.div``;
 
 const HeaderSocial = styled(Social)`
-  position: absolute;
-  top: 4vw;
-  right: 4vw;
+	position: absolute;
+	top: 4vw;
+	right: 4vw;
 
-  ${screenSize.sm} {
-    display: none;
-  }
-`
+	${screenSize.sm} {
+		display: none;
+	}
+`;
 
 const Nav = styled.ul`
-  width: 60%;
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  justify-content: space-between;
-  padding: 0;
-  flex-wrap: wrap;
+	width: 60%;
+	margin-left: auto;
+	margin-right: auto;
+	display: flex;
+	justify-content: space-between;
+	padding: 0;
+	flex-wrap: wrap;
 
-  li {
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    list-style-type: none;
-    border-radius: 100%;
-    border: 1.5px solid #272525;
-    margin: 0 2px;
+	li {
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		list-style-type: none;
+		border-radius: 100%;
+		border: 1.5px solid #272525;
+		margin: 0 2px;
 
-    a {
-      display: flex;
-      box-sizing: border-box;
-      align-items: center;
-      justify-content: center;
-      text-decoration: none;
-      text-transform: uppercase;
-      font-weight: 400;
-      font-size: 0.875rem;
-      text-align: center;
-      color: inherit;
-      width: 80px;
-      height: 80px;
-      padding: 2px;
-    }
+		a {
+			display: flex;
+			box-sizing: border-box;
+			align-items: center;
+			justify-content: center;
+			text-decoration: none;
+			text-transform: uppercase;
+			font-weight: 400;
+			font-size: 0.875rem;
+			text-align: center;
+			color: inherit;
+			width: 80px;
+			height: 80px;
+			padding: 2px;
+		}
 
-    &:hover {
-      background-color: #272525;
+		&:hover {
+			background-color: #272525;
 
-      a {
-        color: white;
-      }
-    }
-  }
+			a {
+				color: white;
+			}
+		}
+	}
 
-  ${screenSize.sm} {
-    li {
-      margin-bottom: 24px;
-    }
-  }
-`
+	${screenSize.sm} {
+		li {
+			margin-bottom: 24px;
+		}
+	}
+`;
 
 const ListItem = styled.li`
-${p => p.active ? "background-color: black;" : ""}
+	${(p) => (p.active ? 'background-color: #272525;' : '')}
 
-&& a {
-  ${p => p.active ? "color: white;" : ""}
-}
-`
+	&& a {
+		${(p) => (p.active ? 'color: white;' : '')}
+	}
+`;
 
-const ListLink = props => {
-  const [iconActive, setIconActive] = useState(false);
-  
-  return (
-  <ListItem active={iconActive}>
-    <Link to={props.to} getProps={({ isCurrent }) => {
-        isCurrent ? setIconActive(true) : setIconActive(false)
-    }}>{props.children}</Link>
-  </ListItem>
-)}
+const ListLink = (props) => {
+	const [iconActive, setIconActive] = useState(false);
+
+	return (
+		<ListItem active={iconActive}>
+			<Link
+				to={props.to}
+				getProps={({ isCurrent }) => {
+					isCurrent ? setIconActive(true) : setIconActive(false);
+				}}
+			>
+				{props.children}
+			</Link>
+		</ListItem>
+	);
+};
 
 const TruckIcon = styled.div`
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  list-style-type: none;
-  height: 80px;
-  background-image: url("/icons/truck.svg");
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position-y: center;
-  padding: 0 20px 0 40px;
-  margin: 0 0px 0 -20px;
+	box-sizing: border-box;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	list-style-type: none;
+	height: 80px;
+	background-image: url('/icons/truck.svg');
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position-y: center;
+	padding: 0 20px 0 40px;
+	margin: 0 0px 0 -20px;
 
-  a {
-    display: flex;
-    box-sizing: border-box;
-    align-items: center;
-    justify-content: center;
-    text-decoration: none;
-    text-transform: uppercase;
-    font-weight: 400;
-    font-size: 0.875rem;
-    text-align: center;
-    color: inherit;
-    width: 80px;
-    height: 80px;
-    padding: 2px;
-  }
+	a {
+		display: flex;
+		box-sizing: border-box;
+		align-items: center;
+		justify-content: center;
+		text-decoration: none;
+		text-transform: uppercase;
+		font-weight: 400;
+		font-size: 0.875rem;
+		text-align: center;
+		color: inherit;
+		width: 80px;
+		height: 80px;
+		padding: 2px;
+	}
 
-  ${p => p.active ? "background-image: url('/icons/truck-black.svg');" : ""}
+	${(p) => (p.active ? "background-image: url('/icons/truck-black.svg');" : '')}
 
-  a {
-    ${p => p.active ? "color: white;" : ""}
-  }
+	a {
+		${(p) => (p.active ? 'color: white;' : '')}
+	}
 
-  &:hover {
-    background-image: url("/icons/truck-black.svg");
+	&:hover {
+		background-image: url('/icons/truck-black.svg');
 
-    a {
-      color: white;
-    }
-  }
-`
+		a {
+			color: white;
+		}
+	}
+`;
 
-const TruckLink = props => {
-  const [iconActive, setIconActive] = useState(false);
-  
-  return (
-  <TruckIcon active={iconActive}>
-      <Link to={props.to} getProps={({ isCurrent }) => {
-        isCurrent ? setIconActive(true) : setIconActive(false)
-    }}>{props.children}</Link>
-  </TruckIcon>
-)}
+const TruckLink = (props) => {
+	const [iconActive, setIconActive] = useState(false);
+
+	return (
+		<TruckIcon active={iconActive}>
+			<Link
+				to={props.to}
+				getProps={({ isCurrent }) => {
+					isCurrent ? setIconActive(true) : setIconActive(false);
+				}}
+			>
+				{props.children}
+			</Link>
+		</TruckIcon>
+	);
+};
+
+const ShopLink = styled.li`
+	background-color: #272525;
+	background-image: url('/icons/e_pood_1.png');
+	background-size: contain;
+
+	a {
+		opacity: 0;
+	}
+
+	:hover {
+		background-image: url('/icons/e_pood_2.png');
+	}
+`;
 
 export default function Header() {
-  const data = useStaticQuery(graphql`
-    query NavQuery {
-      allMarkdownRemark(
-        filter: { frontmatter: { slug: { ne: "home" }, in_nav: { ne: false } } }
-        sort: { order: ASC, fields: frontmatter___order }
-      ) {
-        edges {
-          node {
-            frontmatter {
-              title
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
+	const data = useStaticQuery(graphql`
+		query NavQuery {
+			allMarkdownRemark(
+				filter: { frontmatter: { slug: { ne: "home" }, in_nav: { ne: false } } }
+				sort: { order: ASC, fields: frontmatter___order }
+			) {
+				edges {
+					node {
+						frontmatter {
+							title
+							slug
+						}
+					}
+				}
+			}
+		}
+	`);
 
-  return (
-    <Container>
-      <HeaderSocial color="#272525" />
-      <TopBar />
-      <Nav>
-        {data.allMarkdownRemark.edges.map(({ node }, index) => {
-          if (node.frontmatter.slug === "foodtruck") {
-            return (
-              <TruckLink to={"/" + node.frontmatter.slug} key={index}>
-                {node.frontmatter.title}
-              </TruckLink>
-            )
-          } else {
-            return (
-              <ListLink to={"/" + node.frontmatter.slug} key={index}>
-                {node.frontmatter.title}
-              </ListLink>
-            )
-          }
-        })}
-      </Nav>
-    </Container>
-  )
+	return (
+		<Container>
+			<HeaderSocial color="#272525" />
+			<TopBar />
+			<Nav>
+				<ShopLink>
+					<a href={'https://pood.uulits.ee/'} target="_blank">
+						E-pood
+					</a>
+				</ShopLink>
+				{data.allMarkdownRemark.edges.map(({ node }, index) => {
+					if (node.frontmatter.slug === 'foodtruck') {
+						return (
+							<TruckLink to={'/' + node.frontmatter.slug} key={index}>
+								{node.frontmatter.title}
+							</TruckLink>
+						);
+					} else {
+						return (
+							<ListLink to={'/' + node.frontmatter.slug} key={index}>
+								{node.frontmatter.title}
+							</ListLink>
+						);
+					}
+				})}
+			</Nav>
+		</Container>
+	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,121 +1,122 @@
-import React from "react"
-import styled from "@emotion/styled"
-import { graphql } from "gatsby"
-import { screenSize } from "../styles/screenSize"
+import React from 'react';
+import styled from '@emotion/styled';
+import { graphql } from 'gatsby';
+import { screenSize } from '../styles/screenSize';
 
-import Layout from "../components/layout"
-import CrossSeparator from "../components/CrossSeparator"
-import SEO from "../components/SEO"
+import Layout from '../components/layout';
+import CrossSeparator from '../components/CrossSeparator';
+import SEO from '../components/SEO';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 32px 0 56px 0;
-`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 32px 0 56px 0;
+	overflow-x: hidden;
+`;
 
 const Banner = styled.img`
-  width: 100vw;
-  margin-bottom: 56px;
-  border-top: 5px solid black;
-`
+	width: 100vw;
+	margin-bottom: 56px;
+	border-top: 5px solid black;
+`;
 
 const TextContainer = styled.div`
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 750px;
-  width: 100%;
-  padding: 0 16px;
-  line-height: 1.5;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	max-width: 750px;
+	width: 100%;
+	padding: 0 16px;
+	line-height: 1.5;
 
-  h1 {
-    display: inline-block;
-    margin-top: 0;
+	h1 {
+		display: inline-block;
+		margin-top: 0;
 
-    &::after {
-      content: "";
-      position: absolute;
-      top: 90%;
-      left: 0%;
-      border-bottom: 5px solid #272525;
-      width: 300px;
-      transform: rotate(-6.9deg);
-    }
-  }
+		&::after {
+			content: '';
+			position: absolute;
+			top: 90%;
+			left: 0%;
+			border-bottom: 5px solid #272525;
+			width: 300px;
+			transform: rotate(-6.9deg);
+		}
+	}
 
-  p:first-of-type {
-    margin-top: 0;
-    text-align: justify;
-  }
+	p:first-of-type {
+		margin-top: 0;
+		text-align: justify;
+	}
 
-  & > p {
-    &:first-child::first-letter {
-      font-family: "Teko", Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI",
-        Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-      font-size: 48px;
-      line-height: 0;
-    }
-  }
+	& > p {
+		&:first-child::first-letter {
+			font-family: 'Teko', Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans',
+				'Helvetica Neue', sans-serif;
+			font-size: 48px;
+			line-height: 0;
+		}
+	}
 
-  blockquote {
-    margin: 0;
-    margin-top: 40px;
-    width: 100%;
+	blockquote {
+		margin: 0;
+		margin-top: 40px;
+		width: 100%;
 
-    & > p {
-      text-align: left !important;
-    }
+		& > p {
+			text-align: left !important;
+		}
 
-    ${screenSize.sm} {
-      margin-top: 24px;
-    }
-  }
+		${screenSize.sm} {
+			margin-top: 24px;
+		}
+	}
 
-  blockquote p {
-    font-family: "Teko", Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI",
-      Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    font-size: 56px;
-    line-height: 1;
-    margin-bottom: 0;
+	blockquote p {
+		font-family: 'Teko', Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans',
+			'Helvetica Neue', sans-serif;
+		font-size: 56px;
+		line-height: 1;
+		margin-bottom: 0;
 
-    ${screenSize.sm} {
-      font-size: 40px;
-    }
-  }
+		${screenSize.sm} {
+			font-size: 40px;
+		}
+	}
 
-  img {
-    width: 320px;
-  }
-`
+	img {
+		width: 320px;
+	}
+`;
 
 export default function Home({ data }) {
-  const { markdownRemark } = data
-  const { frontmatter, html } = markdownRemark
+	const { markdownRemark } = data;
+	const { frontmatter, html } = markdownRemark;
 
-  return (
-    <>
-      <SEO title={frontmatter.title} />
-      <Layout>
-        <Container>
-          <Banner src="uploads/front-banner.png" />
-          <CrossSeparator />
-          <TextContainer dangerouslySetInnerHTML={{ __html: html }} />
-        </Container>
-      </Layout>
-    </>
-  )
+	return (
+		<>
+			<SEO title={frontmatter.title} />
+			<Layout>
+				<Container>
+					<Banner src="uploads/front-banner.png" />
+					<CrossSeparator />
+					<TextContainer dangerouslySetInnerHTML={{ __html: html }} />
+				</Container>
+			</Layout>
+		</>
+	);
 }
 
 export const HomepageQuery = graphql`
-  query HompageQuery {
-    markdownRemark(frontmatter: { slug: { eq: "home" } }) {
-      frontmatter {
-        title
-        slug
-      }
-      html
-    }
-  }
-`
+	query HompageQuery {
+		markdownRemark(frontmatter: { slug: { eq: "home" } }) {
+			frontmatter {
+				title
+				slug
+			}
+			html
+		}
+	}
+`;
