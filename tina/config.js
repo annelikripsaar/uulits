670addc3f5932e317587636a3a964161f88092b3
Anneli Kripsaar <annelikripsaar@gmail.com>
2023-04-09 18:33:36 +0300

    Add config

--- tina/config.js
+++ tina/config.js
@@ -1,112 +1,112 @@
-import { defineConfig } from "tinacms";
-import { alamen__Fields } from "./templates";
-import { kontaktFields } from "./templates";
-import { men__Fields } from "./templates";
-import { food_truck_lehtFields } from "./templates";
-import { tavaline_lehtFields } from "./templates";
+import { defineConfig } from 'tinacms';
+import { alamen__Fields } from './templates';
+import { kontaktFields } from './templates';
+import { men__Fields } from './templates';
+import { food_truck_lehtFields } from './templates';
+import { tavaline_lehtFields } from './templates';
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main';
 
 export default defineConfig({
-  branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
-  client: { skip: true },
-  build: {
-    outputFolder: "admin",
-    publicFolder: "static",
-  },
-  media: {
-    tina: {
-      mediaRoot: "",
-      publicFolder: "static",
-    },
-  },
-  schema: {
-    collections: [
-      {
-        format: "md",
-        label: "Lehed",
-        name: "lehed",
-        path: "content",
-        match: {
-          include: "**/*",
-          exclude: "!(menyy)/*",
-        },
-        templates: [
-          {
-            fields: [
-              {
-                type: "rich-text",
-                name: "body",
-                label: "Body of Document",
-                description: "This is the markdown body",
-                isBody: true,
-              },
-              ...tavaline_lehtFields(),
-            ],
-            label: "tavaline-leht",
-            name: "tavaline_leht",
-          },
-          {
-            fields: [
-              {
-                type: "rich-text",
-                name: "body",
-                label: "Body of Document",
-                description: "This is the markdown body",
-                isBody: true,
-              },
-              ...alamen__Fields(),
-            ],
-            label: "alamenuu",
-            name: "alamenuu",
-          },
-          {
-            fields: [
-              {
-                type: "rich-text",
-                name: "body",
-                label: "Body of Document",
-                description: "This is the markdown body",
-                isBody: true,
-              },
-              ...men__Fields(),
-            ],
-            label: "menus",
-            name: "menus",
-          },
-          {
-            fields: [
-              {
-                type: "rich-text",
-                name: "body",
-                label: "Body of Document",
-                description: "This is the markdown body",
-                isBody: true,
-              },
-              ...kontaktFields(),
-            ],
-            label: "kontakt",
-            name: "kontakt",
-          },
-          {
-            fields: [
-              {
-                type: "rich-text",
-                name: "body",
-                label: "Body of Document",
-                description: "This is the markdown body",
-                isBody: true,
-              },
-              ...food_truck_lehtFields(),
-            ],
-            label: "pages",
-            name: "pages",
-          },
-        ],
-      },
-    ],
-  },
+	branch,
+	clientId: process.env.TINA_CLIENT_ID,
+	token: process.env.TINA_TOKEN,
+	client: { skip: true },
+	build: {
+		outputFolder: 'admin',
+		publicFolder: 'static',
+	},
+	media: {
+		tina: {
+			mediaRoot: '',
+			publicFolder: 'static',
+		},
+	},
+	schema: {
+		collections: [
+			{
+				format: 'md',
+				label: 'Lehed',
+				name: 'lehed',
+				path: 'content',
+				match: {
+					include: '**/*',
+					exclude: '!(menyy)/*',
+				},
+				templates: [
+					{
+						fields: [
+							{
+								type: 'rich-text',
+								name: 'body',
+								label: 'Body of Document',
+								description: 'This is the markdown body',
+								isBody: true,
+							},
+							...tavaline_lehtFields(),
+						],
+						label: 'tavaline-leht',
+						name: 'tavaline_leht',
+					},
+					{
+						fields: [
+							{
+								type: 'rich-text',
+								name: 'body',
+								label: 'Body of Document',
+								description: 'This is the markdown body',
+								isBody: true,
+							},
+							...alamen__Fields(),
+						],
+						label: 'alamenuu',
+						name: 'alamenuu',
+					},
+					{
+						fields: [
+							{
+								type: 'rich-text',
+								name: 'body',
+								label: 'Body of Document',
+								description: 'This is the markdown body',
+								isBody: true,
+							},
+							...men__Fields(),
+						],
+						label: 'menus',
+						name: 'menus',
+					},
+					{
+						fields: [
+							{
+								type: 'rich-text',
+								name: 'body',
+								label: 'Body of Document',
+								description: 'This is the markdown body',
+								isBody: true,
+							},
+							...kontaktFields(),
+						],
+						label: 'kontakt',
+						name: 'kontakt',
+					},
+					{
+						fields: [
+							{
+								type: 'rich-text',
+								name: 'body',
+								label: 'Body of Document',
+								description: 'This is the markdown body',
+								isBody: true,
+							},
+							...food_truck_lehtFields(),
+						],
+						label: 'pages',
+						name: 'pages',
+					},
+				],
+			},
+		],
+	},
 });
