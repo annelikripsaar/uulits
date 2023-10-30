e198a448c36cdb3e49011547294d7aa02b2c7146
katrinpaal <149160346+katrinpaal@users.noreply.github.com>
2023-10-30 17:37:03 +0200

    Update head.jsx

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,6 +1,18 @@
 import React from "react"
 import Helmet from "react-helmet"
 
+<head>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EBF6K6EEWH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EBF6K6EEWH');
+</script>
+</head>
+  
 export default function Head() {
   return (
     <Helmet>
